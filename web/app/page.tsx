953ab3e6aeebe29d28ac96127e5b82f077fbb8f8
Nilesh <nilesh.28.2003@gmail.com>
2025-02-21 16:31:49 +0530

    Refactor HomePage component to redirect authenticated users and display loading state

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,31 +1,18 @@
 'use client';
-import { Button } from '@/components/ui/button';
-import { signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import Loading from './loading';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function HomePage() {
 	const { data: session } = useSession();
+	const router = useRouter();
 
-	if (session?.user) {
-		return (
-			<div className='flex h-screen flex-col items-center justify-center gap-4'>
-				<div className='flex flex-col items-center gap-2'>
-					<div>{session?.user?.id}</div>
-					<div>{session?.user?.name}</div>
-					<div>{session?.user?.email}</div>
-				</div>
-				<Button onClick={() => signOut()}>Logout</Button>
-			</div>
-		);
-	}
-	return (
-		<div className='flex h-screen items-center justify-center gap-4'>
-			<Link href='/login'>
-				<Button>Login</Button>
-			</Link>
-			<Link href='/register'>
-				<Button>Register</Button>
-			</Link>
-		</div>
-	);
+	useEffect(() => {
+		if (session?.user) {
+			return router.push('/app');
+		}
+	}, [router, session?.user]);
+
+	return <Loading />;
 }
